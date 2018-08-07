cc9fee2931e7aeebcba8dd14cdc0b8654b5fa89a
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-08-07 11:21:27 +0100

    Fix tests

--- test/Test/Schema/Model.hs
+++ test/Test/Schema/Model.hs
@@ -5,11 +5,12 @@ module Test.Schema.Model where
 
 import           Control.Lens
 import           Data.Aeson
-import qualified Data.Schema            as S
-import           Data.Schema.JSON       hiding (int', string', text')
-import qualified Data.Schema.JSON       as JSON
-import           Data.Text              (Text)
-import           Data.Vector            (Vector)
+import qualified Data.Schema             as S
+import           Data.Schema.JSON
+import           Data.Schema.JSON.Simple (JsonField, JsonSchema)
+import qualified Data.Schema.JSON.Simple as JSON
+import           Data.Text               (Text)
+import           Data.Vector             (Vector)
 import           Test.QuickCheck
 import           Test.Schema.QuickCheck
 
@@ -34,13 +35,13 @@ _AdminRole = prism' AdminRole $ \case
     AdminRole x -> Just x
     _           -> Nothing
 
-departmentProp :: JsonField' AdminRole String
-departmentProp = S.field "department" JSON.string' (to department)
+departmentProp :: JsonField AdminRole String
+departmentProp = S.field "department" JSON.string (to department)
 
-subordinateCountProp :: JsonField' AdminRole Int
-subordinateCountProp = S.field "subordinateCount" JSON.int' (to subordinateCount)
+subordinateCountProp :: JsonField AdminRole Int
+subordinateCountProp = S.field "subordinateCount" JSON.int (to subordinateCount)
 
-roleSchema :: JsonSchema' Role
+roleSchema :: JsonSchema Role
 roleSchema = S.oneOf'
            [ S.alt "user" (S.const' UserRole') _UserRole
            , S.alt "admin" (S.record' (AdminRole' <$> departmentProp <*> subordinateCountProp)) _AdminRole
@@ -49,11 +50,11 @@ roleSchema = S.oneOf'
 data Person = Person { personName :: Text, birthDate :: Int, roles :: Vector Role }
   deriving (Eq, Show)
 
-personSchema :: JsonSchema' Person
+personSchema :: JsonSchema Person
 personSchema = S.record'
              ( Person
-             <$> S.field "name" JSON.text' (to personName)
-             <*> S.field "birthDate" JSON.int' (to birthDate)
+             <$> S.field "name" JSON.text (to personName)
+             <*> S.field "birthDate" JSON.int (to birthDate)
              <*> S.field "roles" (S.seq' roleSchema) (to roles)
              )
 
