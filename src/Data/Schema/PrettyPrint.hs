e0ec1e334dce0b93821b6c25fee77d85d4cccb3b
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-08-29 17:07:24 +0100

    add schema pretty printing

---
+++ src/Data/Schema/PrettyPrint.hs
@@ -0,0 +1,11 @@
+module Data.Schema.PrettyPrint where
+
+import qualified Data.Text.Prettyprint.Doc as Doc
+import qualified Data.Text.Prettyprint.Doc.Render.Terminal as Doc
+
+type Doc = Doc.Doc Doc.AnsiStyle
+
+newtype PP a = PP { getDoc :: Doc }
+
+class PrettySchema s where
+  pprintSchema :: s a -> Doc
