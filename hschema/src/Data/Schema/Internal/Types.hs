ebbdcdc12201dab750dca635be08dfcc81dcf9e7
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-08-31 11:53:15 +0100

    Define Functor instance for FieldDef

--- hschema/src/Data/Schema/Internal/Types.hs
+++ hschema/src/Data/Schema/Internal/Types.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE MultiParamTypeClasses     #-}
 {-# LANGUAGE RankNTypes                #-}
 {-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeOperators             #-}
 {-# LANGUAGE TypeSynonymInstances      #-}
 
 module Data.Schema.Internal.Types where
@@ -14,6 +15,7 @@ import           Control.Applicative.Free
 import           Control.Functor.HigherOrder
 import           Control.Lens                hiding (iso)
 import qualified Control.Lens                as Lens
+import           Control.Natural
 import           Data.Text                   (Text)
 import qualified Data.Text                   as T
 import           Data.Vector                 (Vector)
@@ -29,8 +31,11 @@ data FieldDef o s a = FieldDef
 instance Show (s a) => Show (FieldDef o s a) where
   show f = (T.unpack $ fieldName f) ++ " :: " ++ (show $ fieldSchema f)
 
+instance Functor s => Functor (FieldDef o s) where
+  fmap f (FieldDef name s acc) = FieldDef name (fmap f s) (acc . (to f))
+
 instance HFunctor (FieldDef o) where
-  hfmap nt = \(FieldDef name sch acc) -> FieldDef name (nt sch) acc
+  hfmap nt = \(FieldDef name s acc) -> FieldDef name (nt s) acc
 
 -- | The type of a field of type `a`, belonging to the data type `o` and based on schema `s`
 type Field s o a = Ap (FieldDef o s) a
@@ -62,6 +67,15 @@ instance HFunctor (SchemaF p) where
     RecordSchema fields  -> RecordSchema $ hoistAp (hfmap nt) fields
     UnionSchema alts     -> UnionSchema $ fmap (hfmap nt) alts
     AliasSchema base iso -> AliasSchema (nt base) iso
+
+-- | Perform a natural transformation of the primitive algebra of the Schema
+pfmap :: (p ~> q) -> SchemaF p s a -> SchemaF q s a
+pfmap nt = \case
+  PrimitiveSchema p    -> PrimitiveSchema (nt p)
+  SeqSchema elemSch    -> SeqSchema elemSch
+  RecordSchema fields  -> RecordSchema fields
+  UnionSchema alts     -> UnionSchema alts
+  AliasSchema base iso -> AliasSchema base iso
 
 -- | The Schema type itself for a set of primitives `p` and annotated with `ann`
 type Schema ann p = HCofree (SchemaF p) ann
