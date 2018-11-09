ad53f2ddf3c531dd68c402aaf3f4464d56ab1b32
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-11-09 16:57:05 +0000

    Ignore errors during upload

--- scripts/travis_release.sh
+++ scripts/travis_release.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -e
+set +e
 
 if [[ ! -z "$TRAVIS_TAG" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   echo "Generating source distribution package and uploading it to Hackage..."
   mkdir -p "$HOME/.stack/upload"
   echo "{ \"username\": \"$HACKAGE_USERNAME\", \"password\": \"$HACKAGE_PASSWORD\" }" > "$HOME/.stack/upload/credentials.json"
   stack sdist
+
   stack upload hschema
   stack upload hschema-quickcheck
   stack upload hschema-prettyprinter
